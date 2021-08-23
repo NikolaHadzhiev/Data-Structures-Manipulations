8b916202ca6241a4f481d94322eb5d307431a3d6
NikolaHadzhiev <nikihad2012@abv.bg>
2021-08-23 19:13:15 +0300

    Not weighted graph manipulation

---
+++ Graphs/Not Weighted Graph/NotWeightedGraph.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphProgram
+{
+    public class NotWeightedGraph<T> : Graph<T>
+    {
+        public override void Connect(T parent, T child, int weight = 0)
+        {
+            base.Connect(parent, child, weight);
+            base.Connect(child, parent, weight);
+        }
+    }
+}
---
+++ Graphs/Not Weighted Graph/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GraphProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph<string> cities = new Graph<string>();
+            cities.Add(" S");
+        }
+    }
+}
